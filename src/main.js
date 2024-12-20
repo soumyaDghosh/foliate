afc4b033e76904f1d20604640b2727a58a2e8106
John Factotum <50942278+johnfactotum@users.noreply.github.com>
2024-12-20 18:24:59 +0800

    Release 3.2.1

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import { setConsoleLogDomain } from 'console'
 // mimics (loosely) the `pkg` object set up by GJS if you run `package.init()`
 globalThis.pkg = {
     name: 'com.github.johnfactotum.Foliate',
-    version: '3.2.0',
+    version: '3.2.1',
     MESON,
 }
 pkg.userAgent = `Foliate/${pkg.version}`
